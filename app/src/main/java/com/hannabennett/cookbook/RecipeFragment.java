311b15a04e871978e9a846908ab13a862cb0dc5d
Hanna Bennett <bennetthanna22@gmail.com>
2017-11-08 21:11:06 -0800

    Add text view for ingredients when add ingredient button is clicked

--- app/src/main/java/com/hannabennett/cookbook/RecipeFragment.java
+++ app/src/main/java/com/hannabennett/cookbook/RecipeFragment.java
@@ -11,7 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
 import java.util.UUID;
 
@@ -23,6 +26,7 @@ public class RecipeFragment extends Fragment {
     private static final String ARG_RECIPE_ID = "recipe_id";
 
     private Recipe mRecipe;
+    private LinearLayout mLayout;
     private EditText mTitleField;
     private EditText mIngredientField;
     private RatingBar mRatingBar;
@@ -69,6 +73,28 @@ public class RecipeFragment extends Fragment {
 
             }
         });
+
+        mPhotoButton = (ImageButton) view.findViewById(R.id.recipe_camera);
+        mPhotoView = (ImageView) view.findViewById(R.id.recipe_photo);
+
+        mLayout = (LinearLayout) view.findViewById(R.id.layout);
+        mIngredientField = (EditText) view.findViewById(R.id.recipe_ingredient_edit_text);
+        mAddIngredientButton = (Button) view.findViewById(R.id.recipe_add_ingredient);
+        mAddIngredientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mLayout.addView(createNewTextView(mIngredientField.getText().toString()));
+                mIngredientField.setText("");
+            }
+        });
         return view;
+    }
+
+    private TextView createNewTextView(String text) {
+        final LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(getActivity());
+        textView.setLayoutParams(params);
+        textView.setText(text);
+        return textView;
     }
 }
